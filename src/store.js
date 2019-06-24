6df022a506ce6b754132328c23c4c92e0d449f52
Kateryna Tkachenko <katherinatkachenko@gmail.com>
2019-06-24 04:17:29 +0300

    Added redux-thunk and included to store

--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
-import { createStore} from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import {reducer} from './reducer';
 
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
 export const store = createStore(
-  reducer, 
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  reducer,
+  compose(applyMiddleware(thunk), devTools)
 );
+
 
 
