e1c4f3eb3db88438e3bfeef8eda512ed04ce915a
Kateryna Tkachenko <katherinatkachenko@gmail.com>
2019-06-24 04:00:56 +0300

    Created store using redux and react-redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <Provider store={store}>
+    <App />
+  </Provider>
+), document.getElementById('root'));
 
 serviceWorker.unregister();
--- src/store.js
+++ src/store.js
@@ -1,22 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import { createStore} from 'redux';
-import {Provider} from 'react-redux';
 import {reducer} from './reducer';
-import App from './App';
 
-
-const store = createStore(
+export const store = createStore(
   reducer, 
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-console.log(window.__REDUX_DEVTOOLS_EXTENSION__);
 
-const appElement = document.getElementById('root');
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  appElement
-);
