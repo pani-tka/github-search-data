87de692ddae68f199a634166cf009ab8d4cea7a7
Kateryna Tkachenko <katherinatkachenko@gmail.com>
2019-06-25 03:17:32 +0300

    Created component for search and passed data from store

--- src/components/organization-search.js
+++ src/components/organization-search.js
@@ -1,14 +1,57 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {searchTextChanged, fetchOrganizations} from './actions';
 
-export const OrganizationSearch = () =>(
+class OrganizationSearch extends Component {
+  onChange = event => {
+    const organizationName = event.target.value;
+    this.props.searchTextChanged(organizationName);
+  };
+
+  onClick = () => {
+    this.props.fetchOrganizations();
+  };
+
+render() {
+ const {organizationName} = this.props;
+  console.log( this.props );
+ return (
+   <div>
+   <div>Organization search</div>
+   <div>
+    <input
+      type = "text"
+      placehloder = "Type to search..."
+      value = {organizationName}
+      onChange = {this.onChange}
+    />
+    <button
+      type = "button"
+      onClick = {this.onClick}
+    >
+      Search
+    </button>
+   </div>
   
-  <div>
-    <div>Organization search</div>
-    <div>
-      <input placehloder="Type to search..." />
-    </div>
-    <Link to="/organizations/1">Organization 1</Link>
-    <Link to="/organizations/2">Organization 2</Link>
-  </div>
-);
+   <Link to="/organizations/1">Organization 1</Link>
+   <Link to="/organizations/2">Organization 2</Link>
+ </div>
+ )
+}
+}
+
+const mapStateToProps = state => ({
+  organizations: state.organizations
+})
+
+const mapDispatchToProps = {
+  searchTextChanged,
+  fetchOrganizations,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(OrganizationSearch);
+
